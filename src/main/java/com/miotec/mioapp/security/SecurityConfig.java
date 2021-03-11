fb39e65a98093a472427e3e215bb35bb2e4d0d87
GGWP <guilherme.wolff.001@acad.pucrs.br>
2021-03-11 18:33:12 -0300

    deploy 2.0 heroku

--- src/main/java/com/miotec/mioapp/security/SecurityConfig.java
+++ src/main/java/com/miotec/mioapp/security/SecurityConfig.java
@@ -26,14 +26,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        AuthenticationManager authManager = authenticationManager();
 
         http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
                 .and().authorizeRequests()
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .and().csrf().disable()
-                .addFilter(new CorsConfig());
+                .addFilter(new CorsConfig())
+        ;
     }
 
     @Override
