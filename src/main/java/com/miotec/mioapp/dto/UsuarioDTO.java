6db45ea70971fc9a9fa14b2a1609dc94f4ae9780
GGWP <guilherme.wolff.001@acad.pucrs.br>
2021-03-26 15:53:21 -0300

    Implementado verificação de email

--- src/main/java/com/miotec/mioapp/dto/UsuarioDTO.java
+++ src/main/java/com/miotec/mioapp/dto/UsuarioDTO.java
@@ -2,18 +2,23 @@ package com.miotec.mioapp.dto;
 
 import com.miotec.mioapp.domain.Usuario;
 import lombok.Data;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
 
 @Data
 public class UsuarioDTO {
 
+
+
     private String nome;
     private String email;
-    private String senha;
+    private boolean loginManual;
     private long id;
     private LocalDate dataDeNascimento;
+
 
 
     public static UsuarioDTO create(Usuario u){
@@ -27,3 +32,4 @@ public class UsuarioDTO {
         }
     }
 }
+
